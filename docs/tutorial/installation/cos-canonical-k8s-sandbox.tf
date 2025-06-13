8dd132a8958f793a86b0c3ff91cccb1469836eb5
Michael Thamm <mike.thamm@canonical.com>
2025-06-13 12:57:47 -0400

    chore: fmt TF file

--- docs/tutorial/installation/cos-canonical-k8s-sandbox.tf
+++ docs/tutorial/installation/cos-canonical-k8s-sandbox.tf
@@ -1,13 +1,13 @@
 module "cos" {
-  source                        = "git::https://github.com/canonical/observability//terraform/modules/cos"
-  model_name                    = "cos"
-  channel                       = "1/stable"
-  s3_endpoint                   = "http://{{IPADDR}}:8080"
-  s3_password                   = "secret-key"
-  s3_user                       = "access-key"
-  loki_bucket                   = "loki"
-  mimir_bucket                  = "mimir"
-  tempo_bucket                  = "tempo"
-  ssc_channel                   = "1/stable"
-  anti_affinity                 = true
+  source        = "git::https://github.com/canonical/observability//terraform/modules/cos"
+  model_name    = "cos"
+  channel       = "1/stable"
+  s3_endpoint   = "http://{{IPADDR}}:8080"
+  s3_password   = "secret-key"
+  s3_user       = "access-key"
+  loki_bucket   = "loki"
+  mimir_bucket  = "mimir"
+  tempo_bucket  = "tempo"
+  ssc_channel   = "1/stable"
+  anti_affinity = true
 }
