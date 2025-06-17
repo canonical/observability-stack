8d893dfa310c0882f54377cc26a262048353f0bd
Michael Thamm <mike.thamm@canonical.com>
2025-06-17 17:05:08 -0400

    chore: update offers, validate, and format

--- terraform/cos-lite/outputs.tf
+++ terraform/cos-lite/outputs.tf
@@ -2,10 +2,10 @@
 
 output "offers" {
   value = {
-    alertmanager_karma_dashboard = juju_offer.alertmanager-karma-dashboard
-    grafana_dashboards           = juju_offer.grafana-dashboards
-    loki_logging                 = juju_offer.loki-logging
-    mimir_receive_remote_write   = juju_offer.mimir-receive-remote-write
+    alertmanager_karma_dashboard    = juju_offer.alertmanager-karma-dashboard
+    grafana_dashboards              = juju_offer.grafana-dashboards
+    loki_logging                    = juju_offer.loki-logging
+    prometheus_receive_remote_write = juju_offer.prometheus-receive-remote-write
   }
 }
 
@@ -13,12 +13,12 @@ output "offers" {
 
 output "components" {
   value = {
-    alertmanager  = module.alertmanager
-    catalogue     = module.catalogue
-    grafana       = module.grafana
-    loki          = module.loki
-    prometheus    = module.prometheus
-    ssc           = module.ssc
-    traefik       = module.traefik
+    alertmanager = module.alertmanager
+    catalogue    = module.catalogue
+    grafana      = module.grafana
+    loki         = module.loki
+    prometheus   = module.prometheus
+    ssc          = module.ssc
+    traefik      = module.traefik
   }
 }
