0454daa4b15faa68bc902ce6007feaa3e3d495ae
Michael Thamm <mike.thamm@canonical.com>
2025-06-18 15:33:59 -0400

    test: charm confi map

--- terraform/cos/outputs.tf
+++ terraform/cos/outputs.tf
@@ -1,13 +1,13 @@
 # -------------- # Integration offers -------------- #
 
-output "offers" {
-  value = {
-    alertmanager_karma_dashboard = juju_offer.alertmanager-karma-dashboard
-    grafana_dashboards           = juju_offer.grafana-dashboards
-    loki_logging                 = juju_offer.loki-logging
-    mimir_receive_remote_write   = juju_offer.mimir-receive-remote-write
-  }
-}
+# output "offers" {
+#   value = {
+#     alertmanager_karma_dashboard = juju_offer.alertmanager-karma-dashboard
+#     grafana_dashboards           = juju_offer.grafana-dashboards
+#     loki_logging                 = juju_offer.loki-logging
+#     mimir_receive_remote_write   = juju_offer.mimir-receive-remote-write
+#   }
+# }
 
 # -------------- # Submodules -------------- #
 
@@ -15,12 +15,12 @@ output "components" {
   value = {
     alertmanager  = module.alertmanager
     catalogue     = module.catalogue
-    grafana       = module.grafana
-    grafana_agent = module.grafana_agent
-    loki          = module.loki
-    mimir         = module.mimir
-    ssc           = module.ssc
-    tempo         = module.tempo
-    traefik       = module.traefik
+    # grafana       = module.grafana
+    # grafana_agent = module.grafana_agent
+    # loki          = module.loki
+    # mimir         = module.mimir
+    # ssc           = module.ssc
+    # tempo         = module.tempo
+    # traefik       = module.traefik
   }
 }
