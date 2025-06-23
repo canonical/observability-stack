830df0e2298f79768810ee2e7cf91130b5fe435c
Luca Bello <luca.bello@canonical.com>
2025-06-23 16:44:35 +0200

    fix tf formatting

--- docs/tutorial/installation/cos-canonical-k8s-sandbox.tf
+++ docs/tutorial/installation/cos-canonical-k8s-sandbox.tf
@@ -2,7 +2,7 @@ module "cos" {
   source          = "git::https://github.com/canonical/observability-stack//terraform/cos"
   model           = "cos"
   channel         = "1/stable"
-  s3_endpoint     = "http://s3-endpoint:8080"  # Change this to your s3 endpoint
+  s3_endpoint     = "http://s3-endpoint:8080" # Change this to your s3 endpoint
   s3_secret_key   = "secret-key"
   s3_access_key   = "access-key"
   loki_bucket     = "loki"
