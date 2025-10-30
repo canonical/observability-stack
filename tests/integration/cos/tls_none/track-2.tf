01a0e6f93ba92f63de2471a98e2048128562ea7d
Michael Thamm <mike.thamm@canonical.com>
2025-10-30 13:22:56 -0400

    chore

--- tests/integration/cos/tls_none/track-2.tf
+++ tests/integration/cos/tls_none/track-2.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 1.5"
+  required_providers {
+    juju = {
+      source  = "juju/juju"
+      version = "~> 1.0"
+    }
+  }
+}
+
 variable "model" {
   type = string
 }
