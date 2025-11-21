2b4774f8203b2d3ddd805bbe3fded0aa0f181600
Michael Thamm <mike.thamm@canonical.com>
2025-11-21 09:57:43 -0500

    fix: update Grafana's ingress integrations (#149)

--- tests/integration/cos_lite/tls_external/test_upgrade_cos_lite_tls_external.py
+++ tests/integration/cos_lite/tls_external/test_upgrade_cos_lite_tls_external.py
@@ -24,7 +24,6 @@ def test_deploy_from_track(
 ):
     # GIVEN a module deployed from track n-1
     tf_manager.init(TRACK_1_TF_FILE)
-    tf_manager.apply(target="ssc", ca_model=ca_model.model, cos_model=cos_model.model)
     tf_manager.apply(ca_model=ca_model.model, cos_model=cos_model.model)
     wait_for_active_idle_without_error([ca_model, cos_model])
 
