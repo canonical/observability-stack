ce7b1cbd901ef88798766cc5f3faa9e514ecab7c
Michael Thamm <mike.thamm@canonical.com>
2025-11-23 21:12:02 -0500

    chore: Fix tests

--- tests/integration/cos/tls_full/track-2.tf
+++ tests/integration/cos/tls_full/track-2.tf
@@ -48,7 +48,8 @@ module "cos" {
   model_uuid                      = data.juju_model.cos-model.uuid
   channel                         = "2/stable"
   internal_tls                    = "true"
-  external_certificates_offer_url = module.ssc.offers.certificates.url
+  external_certificates_offer_url = "admin/${var.ca_model}.certificates"
+  external_ca_cert_offer_url      = "admin/${var.ca_model}.send-ca-cert"
 
   s3_endpoint   = var.s3_endpoint
   s3_secret_key = var.s3_secret_key
