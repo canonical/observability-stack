ab13c3d1ac1a1ad6280a2aeabe4cbede5cfba5e6
Michael Thamm <mike.thamm@canonical.com>
2025-11-27 16:29:40 -0500

    chore

--- tests/integration/cos_lite/tls_external/track-2.tf
+++ tests/integration/cos_lite/tls_external/track-2.tf
@@ -34,7 +34,7 @@ module "ssc" {
 module "cos-lite" {
   source                          = "git::https://github.com/canonical/observability-stack//terraform/cos-lite?ref=track/2"
   model_uuid                      = data.juju_model.cos-model.uuid
-  channel                         = "2/stable"
+  channel                         = "2/edge"  # TODO: Switch to "2/stable" when Prometheus rev279 hits stable
   internal_tls                    = "false"
   external_certificates_offer_url = "admin/${var.ca_model}.certificates"
   external_ca_cert_offer_url      = "admin/${var.ca_model}.send-ca-cert"
