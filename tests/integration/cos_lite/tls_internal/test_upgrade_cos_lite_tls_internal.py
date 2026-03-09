c5ef01bca1e7d2add1e6b1a6fcfa521ff987051d
Michael Thamm <mike.thamm@canonical.com>
2026-03-09 16:05:47 -0400

    chore: bump timeouts

--- tests/integration/cos_lite/tls_internal/test_upgrade_cos_lite_tls_internal.py
+++ tests/integration/cos_lite/tls_internal/test_upgrade_cos_lite_tls_internal.py
@@ -20,5 +20,5 @@ def test_deploy_from_track(tf_manager, cos_model: jubilant.Juju):
     # GIVEN a module deployed from track n-1
     tf_manager.init(TRACK_2_TF_FILE)
     tf_manager.apply(model=cos_model.model)
-    wait_for_active_idle_without_error([cos_model])
+    wait_for_active_idle_without_error([cos_model], timeout=60*60)
     catalogue_apps_are_reachable(cos_model)
