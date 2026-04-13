47dfc202f2dc9b0a4f64f9b85864ff371148042a
Leon <82407168+sed-i@users.noreply.github.com>
2026-04-13 17:32:07 -0400

    fix indentation
    Signed-off-by: Leon <82407168+sed-i@users.noreply.github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -165,7 +165,7 @@ html_extra_path = []
 # - https://git.launchpad.net/example
 #
 html_theme_options = {
-  'source_edit_link': 'https://github.com/canonical/observability-stack',
+    'source_edit_link': 'https://github.com/canonical/observability-stack',
 }
 
 # Project slug; see https://meta.discourse.org/t/what-is-category-slug/87897
