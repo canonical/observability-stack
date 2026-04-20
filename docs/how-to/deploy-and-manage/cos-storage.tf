5f778acc6693b7f0bdbd48859a817cb38c570f19
Michael Thamm <mike.thamm@canonical.com>
2026-04-20 15:48:27 -0400

    chore: cleaner TF files

--- docs/how-to/deploy-and-manage/cos-storage.tf
+++ docs/how-to/deploy-and-manage/cos-storage.tf
@@ -1,20 +1,20 @@
 module "cos" {
   # Use the right source value depending on whether you are using cos or cos-lite
-  source                          = "git::https://github.com/canonical/observability-stack//terraform/cos?ref=track/2"
-  channel                         = "2/stable"
-  
+  source  = "git::https://github.com/canonical/observability-stack//terraform/cos?ref=track/2"
+  channel = "2/stable"
+
   # ... other inputs ...
 
   # Configure a component's storage directives
   prometheus = {
     backend_storage_directives = {
-      database: "kubernetes,1,10G",  # sizes are just examples, adjust as needed
+      database : "kubernetes,1,10G", # sizes are just examples, adjust as needed
     }
   }
   mimir_worker = {
     backend_storage_directives = {
-      data: "kubernetes,1,10G",  # sizes are just examples, adjust as needed
-      recovery-data: "kubernetes,1,2G"  # sizes are just examples, adjust as needed
+      data : "kubernetes,1,10G",        # sizes are just examples, adjust as needed
+      recovery-data : "kubernetes,1,2G" # sizes are just examples, adjust as needed
     }
   }
 }
--- docs/how-to/deploy-and-manage/cos-tls.tf
+++ docs/how-to/deploy-and-manage/cos-tls.tf
@@ -1,12 +1,12 @@
 module "cos" {
   # Use the right source value depending on whether you are using cos or cos-lite
-  source                          = "git::https://github.com/canonical/observability-stack//terraform/cos?ref=track/2"
-  channel                         = "2/stable"
-  
+  source  = "git::https://github.com/canonical/observability-stack//terraform/cos?ref=track/2"
+  channel = "2/stable"
+
   # ... other inputs ...
 
   # TLS between in-model applications
-  internal_tls                    = true
+  internal_tls = true
   # Update the _offer_url inputs with the offered endpoints of the external CA's model
   external_certificates_offer_url = "admin/external-ca-model.certificates" # Set to 'null' to communicate with Traefik via HTTP, i.e. no 'external_tls'
   external_ca_cert_offer_url      = "admin/external-ca-model.send-ca-cert" # Required if 'external_certificates_offer_url' is set
