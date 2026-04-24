0bb93b1d29a4beeb434513a7293a482747811d53
Michael Thamm <mike.thamm@canonical.com>
2026-04-24 15:48:18 -0400

    chore

--- terraform/cos-lite/upgrades.tf
+++ terraform/cos-lite/upgrades.tf
@@ -8,6 +8,15 @@ resource "terraform_data" "grafana_ingress_interface" {
   triggers_replace = data.juju_charm.grafana_info.requires["ingress"]
 }
 
+## -------- Removed the litestream-image resource ----------
+# The litestream-image resource was removed and given a Juju bug, we need to add a lifecycle to
+# trigger integration replacement, otherwise the upgrade will fail
+# https://github.com/juju/juju/issues/21648
+# https://github.com/juju/juju/issues/22071
+resource "terraform_data" "grafana_litestream_resource" {
+  triggers_replace = contains(keys(data.juju_charm.grafana_info.resources), "litestream-image")
+}
+
 # -------------- # CharmHub API -------------- #
 
 data "juju_charm" "grafana_info" {
