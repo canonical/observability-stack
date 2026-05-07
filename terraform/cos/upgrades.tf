f30a11816d3c984ed69ff9b41b263e5e28addb32
Michael Thamm <mike.thamm@canonical.com>
2026-05-07 09:12:02 -0400

    chore: Remove Traefik patch in upgrades.tf

--- terraform/cos/upgrades.tf
+++ terraform/cos/upgrades.tf
@@ -18,26 +18,10 @@ resource "terraform_data" "grafana_ingress_interface" {
   triggers_replace = lookup(data.juju_charm.grafana_info.requires, "ingress", "")
 }
 
-# -- Traefik -- #
-
-# [integration] Malformed cert on upgrade
-#   When upgraded, traefik's certificate is written to disk with double-quotes
-#   causing 500 errors when using ingress since Traefik doesn't trust the cert.
-#   https://github.com/canonical/traefik-k8s-operator/issues/668
-resource "terraform_data" "traefik_revision" {
-  triggers_replace = data.juju_charm.traefik_info.revision
-}
-
 # -------------- # CharmHub API -------------- #
 
 data "juju_charm" "grafana_info" {
   charm   = "grafana-k8s"
   channel = var.channel
   base    = "ubuntu@24.04"
-}
-
-data "juju_charm" "traefik_info" {
-  charm   = "traefik-k8s"
-  channel = var.traefik.channel
-  base    = "ubuntu@20.04"
 }
