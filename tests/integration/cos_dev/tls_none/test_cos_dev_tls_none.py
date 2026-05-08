82ca8974ec556b08795d965e4e49894a1bbb4848
Michael Thamm <mike.thamm@canonical.com>
2026-05-08 11:39:22 -0400

    feat: Check otelcol logs in COS tests (#170)

--- tests/integration/cos_dev/tls_none/test_cos_dev_tls_none.py
+++ tests/integration/cos_dev/tls_none/test_cos_dev_tls_none.py
@@ -5,9 +5,12 @@ https://documentation.ubuntu.com/observability/latest/how-to/configure-tls-encry
 
 from pathlib import Path
 
-from helpers import catalogue_apps_are_reachable, wait_for_active_idle_without_error
-
 import jubilant
+from helpers import (
+    catalogue_apps_are_reachable,
+    no_errors_in_otelcol_logs,
+    wait_for_active_idle_without_error,
+)
 
 TRACK_DEV_TF_FILE = Path(__file__).parent.resolve() / "track-dev.tf"
 
@@ -17,3 +20,4 @@ def test_deploy(tf_manager, cos_model: jubilant.Juju):
     tf_manager.apply(model=cos_model.model)
     wait_for_active_idle_without_error([cos_model])
     catalogue_apps_are_reachable(cos_model)
+    no_errors_in_otelcol_logs(cos_model)
