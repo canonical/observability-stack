a4a484bd08e18695959783607b8e99ee4dc64688
Michael Thamm <mike.thamm@canonical.com>
2026-05-22 13:49:43 -0400

    chore

--- terraform/cos-lite/applications.tf
+++ terraform/cos-lite/applications.tf
@@ -1,5 +1,6 @@
 module "alertmanager" {
   source             = "git::https://github.com/canonical/alertmanager-k8s-operator//terraform"
+
   app_name           = var.alertmanager.app_name
   channel            = local.channels.alertmanager
   config             = var.alertmanager.config
@@ -12,6 +13,7 @@ module "alertmanager" {
 
 module "catalogue" {
   source             = "git::https://github.com/canonical/catalogue-k8s-operator//terraform"
+
   app_name           = var.catalogue.app_name
   channel            = local.channels.catalogue
   config             = var.catalogue.config
@@ -24,6 +26,7 @@ module "catalogue" {
 
 module "grafana" {
   source             = "git::https://github.com/canonical/grafana-k8s-operator//terraform"
+
   app_name           = var.grafana.app_name
   channel            = local.channels.grafana
   config             = var.grafana.config
@@ -37,6 +40,7 @@ module "grafana" {
 
 module "loki" {
   source             = "git::https://github.com/canonical/loki-k8s-operator//terraform"
+
   app_name           = var.loki.app_name
   channel            = local.channels.loki
   config             = var.loki.config
@@ -49,6 +53,7 @@ module "loki" {
 
 module "prometheus" {
   source             = "git::https://github.com/canonical/prometheus-k8s-operator//terraform"
+
   app_name           = var.prometheus.app_name
   channel            = local.channels.prometheus
   config             = var.prometheus.config
