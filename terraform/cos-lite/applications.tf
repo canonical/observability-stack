1e86db39e6d7ba74a01d24166df360cb64ebb489
Michael Thamm <mike.thamm@canonical.com>
2026-05-27 13:55:52 -0400

    chore

--- terraform/cos-lite/applications.tf
+++ terraform/cos-lite/applications.tf
@@ -59,7 +59,7 @@ module "prometheus" {
 }
 
 module "ssc" {
-  count       = var.internal_tls ? 1 : 0
+  count = var.internal_tls ? 1 : 0
   # The TF module bumped the Juju provider to v2 after this commit, pin for compatibility with provider v1 
   source      = "git::https://github.com/canonical/self-signed-certificates-operator//terraform?ref=0216698683a757a44d02e98c003a19aa7ffcfb63"
   app_name    = var.ssc.app_name
