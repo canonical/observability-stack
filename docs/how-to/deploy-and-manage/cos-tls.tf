95b4863a9d0180ddb3e2da42fb1ef7fab0b17592
Michael Thamm <mike.thamm@canonical.com>
2026-06-03 16:23:41 -0400

    chore

--- docs/how-to/deploy-and-manage/cos-tls.tf
+++ docs/how-to/deploy-and-manage/cos-tls.tf
@@ -1,11 +1,11 @@
 module "cos" {
   # Use the right source value depending on whether you are using cos or cos-lite
-  source       = "git::https://github.com/canonical/observability-stack//terraform/cos?ref=main"
+  source = "git::https://github.com/canonical/observability-stack//terraform/cos?ref=main"
 
   # ... other inputs ...
 
   internal_tls = true # TLS between in-model applications
-  
+
   # Update the _offer_url inputs with the offered endpoints of the external CA's model
   external_certificates_offer_url = "admin/external-ca-model.certificates" # Set to 'null' to communicate with Traefik via HTTP, i.e. no 'external_tls'
   external_ca_cert_offer_url      = "admin/external-ca-model.send-ca-cert" # Required if 'external_certificates_offer_url' is set
