c1047f4a392194660c918dd9e70c351918771070
Michael Thamm <mike.thamm@canonical.com>
2026-06-05 09:36:33 -0400

    chore: remove temp refs in tests

--- tests/integration/cos/tls_internal/track-dev.tf
+++ tests/integration/cos/tls_internal/track-dev.tf
@@ -30,7 +30,7 @@ variable "s3_access_key" {
 }
 
 module "cos" {
-  source       = "git::https://github.com/canonical/observability-stack//terraform/cos?ref=feat/cos-model-resource"
+  source       = "git::https://github.com/canonical/observability-stack//terraform/cos"
   model        = { uuid = data.juju_model.model.uuid }
   risk         = "edge"
   internal_tls = true
