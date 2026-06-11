08d1ceef1d44d05f7df7fbf63ec8ef56d006f524
Michael Thamm <mike.thamm@canonical.com>
2026-06-11 14:44:50 -0400

    feat: Risk upgrades

---
+++ tests/integration/cos_lite/risk_upgrades/test_track_dev_cos_lite_upgrade_risk.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+import jubilant
+from helpers import generic_assertions
+
+TF_FILE = Path(__file__).parent.resolve() / "track-dev-risks.tf"
+
+
+def test_deploy_from_track_2(
+    tf_manager, ca_model: jubilant.Juju, cos_model: jubilant.Juju, tmp_path
+):
+    # GIVEN a module deployed with a "stable" risk
+    tf_manager.init(TF_FILE)
+    tf_manager.apply(ca_model=ca_model.model, cos_model=cos_model.model, risk="stable")
+    generic_assertions(cos_model, ca_model, tmp_path)
+
+
+def test_upgrade_path(
+    tf_manager, ca_model: jubilant.Juju, cos_model: jubilant.Juju, tmp_path
+):
+    # WHEN upgraded to higher risk
+    for risk in ["candidate", "beta", "edge"]:
+        tf_manager.init(TF_FILE)
+        tf_manager.apply(ca_model=ca_model.model, cos_model=cos_model.model, risk=risk)
+
+        # THEN the model is upgraded and is healthy
+        generic_assertions(cos_model, ca_model, tmp_path)
