23ed0a56b0c340fb03276e25b85bd961a269c5e6
Michael Thamm <mike.thamm@canonical.com>
2026-07-01 19:03:40 -0400

    chore

--- tests/integration/cos/tls_internal/track-3.0.tf
+++ tests/integration/cos/tls_internal/track-3.0.tf
@@ -31,7 +31,7 @@ variable "s3_access_key" {
 
 module "cos" {
   source       = "git::https://github.com/canonical/observability-stack//terraform/cos?ref=track/3.0"
-  model_uuid   = data.juju_model.model.uuid
+  model        = { uuid = data.juju_model.model.uuid }
   risk         = "edge"
   internal_tls = true
 
--- tests/integration/cos_lite/tls_internal/track-3.0.tf
+++ tests/integration/cos_lite/tls_internal/track-3.0.tf
@@ -19,7 +19,7 @@ data "juju_model" "model" {
 
 module "cos-lite" {
   source       = "git::https://github.com/canonical/observability-stack//terraform/cos-lite?ref=track/3.0"
-  model_uuid   = data.juju_model.model.uuid
+  model        = { uuid = data.juju_model.model.uuid }
   risk         = "edge"
   internal_tls = true
 }
