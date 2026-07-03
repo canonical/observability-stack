3d4c1967628ea075b9bb93bd21dfbdd7b9e5ebf1
Mateusz Kulewicz <mateusz.kulewicz@canonical.com>
2026-07-03 09:15:30 +0200

    fix: Add files from 3.0 track that are included

---
+++ tests/integration/cos_lite/tls_internal/track-3.0.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 1.5"
+  required_providers {
+    juju = {
+      source  = "juju/juju"
+      version = ">= 1.0"
+    }
+  }
+}
+
+variable "model" {
+  type = string
+}
+
+data "juju_model" "model" {
+  name  = var.model
+  owner = "admin"
+}
+
+module "cos-lite" {
+  source       = "git::https://github.com/canonical/observability-stack//terraform/cos-lite"
+  model        = { uuid = data.juju_model.model.uuid }
+  internal_tls = true
+}
