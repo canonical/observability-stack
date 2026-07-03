de91c2ced4cdf53b834884f57d729c74cf5341f8
Mateusz Kulewicz <mateusz.kulewicz@canonical.com>
2026-07-03 16:14:12 +0200

    temp: Pin tracks to this branch to show CI

--- tests/integration/cos_lite/tls_internal/track-dev.tf
+++ tests/integration/cos_lite/tls_internal/track-dev.tf
@@ -18,7 +18,7 @@ data "juju_model" "model" {
 }
 
 module "cos-lite" {
-  source       = "git::https://github.com/canonical/observability-stack//terraform/cos-lite"
+  source       = "git::https://github.com/canonical/observability-stack//terraform/cos-lite?ref=chore/update-references"
   model        = { uuid = data.juju_model.model.uuid }
   risk         = "edge"
   internal_tls = true
