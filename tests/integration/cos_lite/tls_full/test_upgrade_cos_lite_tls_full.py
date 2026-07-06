11fd2cc310a1eccec7de98c38ee09ccca0301874
Michael Thamm <mike.thamm@canonical.com>
2026-07-06 16:26:04 -0400

    chore

--- tests/integration/cos_lite/tls_full/test_upgrade_cos_lite_tls_full.py
+++ tests/integration/cos_lite/tls_full/test_upgrade_cos_lite_tls_full.py
@@ -9,28 +9,16 @@ from pathlib import Path
 import jubilant
 from helpers import generic_assertions
 
-TRACK_2_TF_FILE = Path(__file__).parent.resolve() / "track-2.tf"
 TRACK_3_TF_FILE = Path(__file__).parent.resolve() / "track-3.0.tf"
 TRACK_DEV_TF_FILE = Path(__file__).parent.resolve() / "track-dev.tf"
 
 
-def test_deploy_from_track_2(
+def test_deploy_from_track(
     tmp_path, tf_manager, ca_model: jubilant.Juju, cos_model: jubilant.Juju
 ):
-    # GIVEN a module deployed from track 2
-    tf_manager.init(TRACK_2_TF_FILE)
-    tf_manager.apply(ca_model=ca_model.model, cos_model=cos_model.model)
-    generic_assertions(cos_model, ca_model, tmp_path)
-
-
-def test_deploy_to_track_3(
-    tmp_path, tf_manager, ca_model: jubilant.Juju, cos_model: jubilant.Juju
-):
-    # WHEN upgraded to track 3.0
+    # GIVEN a module deployed from the previous track
     tf_manager.init(TRACK_3_TF_FILE)
     tf_manager.apply(ca_model=ca_model.model, cos_model=cos_model.model)
-
-    # THEN the model is upgraded and is healthy
     generic_assertions(cos_model, ca_model, tmp_path)
 
 
