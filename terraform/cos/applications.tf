d58081400d6124c7d37a78ee43897cd33e7f5e0b
Michael Thamm <mike.thamm@canonical.com>
2026-07-06 10:06:49 -0400

    chore

--- terraform/cos/applications.tf
+++ terraform/cos/applications.tf
@@ -14,7 +14,7 @@ module "alertmanager" {
 }
 
 module "catalogue" {
-  source = "git::https://github.com/canonical/catalogue-k8s-operator//terraform?ref=tf-3.0.2"
+  source = "git::https://github.com/canonical/catalogue-k8s-operator//terraform?ref=tf-3.0.3"
 
   app_name           = var.catalogue.app_name
   base               = local.bases.o11y
