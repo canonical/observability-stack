39bb4b94516355d539e1edb48da8c0a97ddd7290
Michael Thamm <mike.thamm@canonical.com>
2026-07-07 08:40:06 -0400

    chore: Remove the track/3.0 upgrades (#440)

--- tests/integration/cos_lite/tls_external/test_upgrade_cos_lite_tls_external.py
+++ tests/integration/cos_lite/tls_external/test_upgrade_cos_lite_tls_external.py
@@ -11,34 +11,22 @@ from helpers import generic_assertions
 
 TRACK_2_TF_FILE = Path(__file__).parent.resolve() / "track-2.tf"
 TRACK_3_TF_FILE = Path(__file__).parent.resolve() / "track-3.0.tf"
-TRACK_DEV_TF_FILE = Path(__file__).parent.resolve() / "track-dev.tf"
 
 
-def test_deploy_from_track_2(
+def test_deploy_from_track(
     tmp_path, tf_manager, ca_model: jubilant.Juju, cos_model: jubilant.Juju
 ):
-    # GIVEN a module deployed from track 2
+    # GIVEN a module deployed from the previous track
     tf_manager.init(TRACK_2_TF_FILE)
     tf_manager.apply(ca_model=ca_model.model, cos_model=cos_model.model)
     generic_assertions(cos_model, ca_model, tmp_path)
 
 
-def test_deploy_to_track_3(
+def test_deploy_to_track(
     tmp_path, tf_manager, ca_model: jubilant.Juju, cos_model: jubilant.Juju
 ):
-    # WHEN upgraded to track 3.0
+    # WHEN upgraded to the next track
     tf_manager.init(TRACK_3_TF_FILE)
-    tf_manager.apply(ca_model=ca_model.model, cos_model=cos_model.model)
-
-    # THEN the model is upgraded and is healthy
-    generic_assertions(cos_model, ca_model, tmp_path)
-
-
-def test_deploy_to_track_dev(
-    tmp_path, tf_manager, ca_model: jubilant.Juju, cos_model: jubilant.Juju
-):
-    # WHEN upgraded to track dev
-    tf_manager.init(TRACK_DEV_TF_FILE)
     tf_manager.apply(ca_model=ca_model.model, cos_model=cos_model.model)
 
     # THEN the model is upgraded and is healthy
