fa8127e74250dcb70a0a6647dd0ca7e1994b56b3
Sina <sina.pahlavan@canonical.com>
2026-09-25 11:52:09 -0400

    fix: explicitly offer external SSC endpoints

--- docs/how-to/deploy-and-manage/cos-tls.tf
+++ docs/how-to/deploy-and-manage/cos-tls.tf
@@ -6,7 +6,9 @@ module "cos" {
 
   internal_tls = true # TLS between in-model applications
 
-  # Update the _offer_url inputs with the offered endpoints of the external CA's model
-  external_certificates_offer_url = "admin/external-ca-model.certificates" # Set to 'null' to communicate with Traefik via HTTP, i.e. no 'external_tls'
-  external_ca_cert_offer_url      = "admin/external-ca-model.send-ca-cert" # Required if 'external_certificates_offer_url' is set
+  # Update the _offer_url inputs with the offered endpoints of the external CA's model.
+  # Offers are now opt-in and named <app_name>-<endpoint>, so enable them on the CA module with
+  # offered_endpoints = ["certificates", "send-ca-cert"] (app_name "self-signed-certificates" below).
+  external_certificates_offer_url = "admin/external-ca-model.self-signed-certificates-certificates" # Set to 'null' to communicate with Traefik via HTTP, i.e. no 'external_tls'
+  external_ca_cert_offer_url      = "admin/external-ca-model.self-signed-certificates-send-ca-cert" # Required if 'external_certificates_offer_url' is set
 }
--- tests/integration/cos_lite/tls_external/track-dev.tf
+++ tests/integration/cos_lite/tls_external/track-dev.tf
@@ -29,6 +29,8 @@ data "juju_model" "cos-model" {
 module "ssc" {
   source     = "git::https://github.com/canonical/self-signed-certificates-operator//terraform"
   model_uuid = data.juju_model.ca-model.uuid
+
+  offered_endpoints = ["certificates", "send-ca-cert"]
 }
 
 module "cos-lite" {
@@ -38,6 +40,6 @@ module "cos-lite" {
   model                           = { uuid = data.juju_model.cos-model.uuid }
   risk                            = "edge"
   internal_tls                    = false
-  external_certificates_offer_url = "admin/${var.ca_model}.certificates"
-  external_ca_cert_offer_url      = "admin/${var.ca_model}.send-ca-cert"
+  external_certificates_offer_url = "admin/${var.ca_model}.self-signed-certificates-certificates"
+  external_ca_cert_offer_url      = "admin/${var.ca_model}.self-signed-certificates-send-ca-cert"
 }
